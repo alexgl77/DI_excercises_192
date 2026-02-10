03fc17a4899aef6229b38d1d8721df5754132b65
alexgl77 <agoldbaum@fen.uchile.cl>
2026-02-10 10:59:07 +0200

    Remove comment descriptions

--- DI_excercises_192/Exercise_dic.py
+++ DI_excercises_192/Exercise_dic.py
@@ -1,23 +1,23 @@
-# Exercise 1: List of tuples to dictionary (comprehension)
+# Exercise 1: 
 my_list = [("name", "Elie"), ("job", "Instructor")]
 person_dict = {key: value for key, value in my_list}
 print(person_dict)
 
-# Exercise 2: Two lists to dictionary (comprehension)
+# Exercise 2: 
 states_ab = ["Ca", "NY", "RI"]
 states_names = ["California", "New York", "Rhode Island"]
 states_dict = {key: value for key, value in zip(states_ab, states_names)}
 print(states_dict)
 
-# Exercise 3: Vowels with value 0
+# Exercise 3: 
 vowel_map = {x: 0 for x in "aeiou"}
 print(vowel_map)
 
-# Exercise 4: Alphabet dictionary
+# Exercise 4: 
 alphabet_dict = {x: chr(x + 64) for x in range(1, 27)}
 print(alphabet_dict)
 
-# Exercise 5: Count vowels only
+# Exercise 5: 
 string = "awesome sauce"
 vowel_count = {x: string.count(x) for x in "aeiou"}
 print(vowel_count)
